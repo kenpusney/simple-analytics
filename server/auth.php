cf2d6e5237ff6cadc73bf64c5d3ec872b869c411
Kimmy Leo <kenpusney@gmail.com>
2018-03-11 17:21:00 +0800

    add qunachi

--- server/auth.php
+++ server/auth.php
@@ -14,5 +14,6 @@ function registered($id) {
         '113d35e1813375dd6b59393bb61cc5698498ecfece53b21c70a5fa738efd89c2', // net.kimleo.portal
         'd1e76c62d60e34e7401b9f57a313f9492d9a5b1ca51d5289ec4277dc3c2383fa', // net.kimleo.spec
         'c08aec1673d18bc9084cddf8744ed0cb3b4ff4351a1d89e6a9382fb3106b3fb7', // net.kimleo.badges
+        '96b234540af30880fb68e0ffa3080a4119307580eccd272e4a0961a6e3fd3690', // net.kimleo.qunachi
     ));
 }
