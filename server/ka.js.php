5815cb2e18d080c6fd614050fd2c1bf26211cee1
Kimmy Leo <kenpusney@gmail.com>
2018-03-11 17:16:01 +0800

    add more functions to support analysis hook

--- server/ka.js.php
+++ server/ka.js.php
@@ -3,7 +3,8 @@ require_once("common.php");
 $id = getOrDefault("id", "default");
 $host = $_SERVER["HTTP_HOST"];
 ?>
-(function (global) {
-    fetch("http://<?= $host ?>/ka.php?id=<?= $id ?>&origin=" +
-        encodeURIComponent(location.href));
+(function(g){
+e=encodeURIComponent;f=fetch;b="http://<?= $host ?>/ka.php?id=<?= $id ?>&origin="+e(location.href);
+f(b);
+g.ka=function(o){p="";for(k in o){if(o.hasOwnProperty(k)&&k!=='id'&&k!=='origin'){p += ("&" + e(k) + "=" + e(o[k]));}};f(b + p);}
 }(this));
